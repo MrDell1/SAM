a8e5fee2c097677eb2551597ce4afbb8d8132105
Marcin Leszczyński <33961381+MrDell1@users.noreply.github.com>
2023-03-20 14:47:56 +0100

    change functions

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -2,15 +2,9 @@ const express = require('express')
 
 const app = express()
 
-function audioCancel(){
-    const audioPlayer = document.getElementById("audioPlayer");
-    audioPlayer.src="cancel.mp3";
-}
 
-function videoCancel(){
-    const videoCancel = document.getElementById("videoCancel");
-    videoCancel.src="cancel.mp4";
-}
+
+
 
 app.get('/', (req, res) => {
     response = {  
@@ -20,10 +14,16 @@ app.get('/', (req, res) => {
    };
    let msg = '';
    if(response.videFile){
-   	msg += `<video id="videoPlayer" src=${response.videFile}></video><br><button id="videoCancel" onClick="videoCancel()">Video Audio</button><br>`;
+   	msg += `<video id="videoPlayer" src=${response.videFile}></video><br><button id="videoCancel" onClick="videoCancel()">Video Audio</button><br><script>function videoCancel(){
+        const videoCancel = document.getElementById("videoCancel");
+        videoCancel.src="cancel.mp4";
+    }</script>`;
    }
    if(response.audioFile){
-   	msg += `<audio id="audioPlayer" src=${response.audioFile}></audio><br><button id="audioCancel" onClick="audioCancel()">Cancel Audio</button><br>`;
+   	msg += `<audio id="audioPlayer" src=${response.audioFile}></audio><br><button id="audioCancel" onClick="audioCancel()">Cancel Audio</button><br><script>function audioCancel(){
+        const audioPlayer = document.getElementById("audioPlayer");
+        audioPlayer.src="cancel.mp3";
+    }</script>`;
    }
    if(response.imgFile){
     msg += `<img id="posterImage" src=${response.imgFile}></img><br>`;
