1a8971db5a51e23a84dff803f59bd2397edd7722
Marcin Leszczyński <33961381+MrDell1@users.noreply.github.com>
2023-03-20 15:17:20 +0100

    add Hello World text

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -3,16 +3,13 @@ const express = require('express')
 const app = express()
 
 
-
-
-
 app.get('/', (req, res) => {
     response = {  
        videoFile:req.query.videoFile,  
        audioFile:req.query.audioFile,
        imgFile:req.query.imgFile
    };
-   let msg = '';
+   let msg = '<h1>Hello World!</h1><br>';
    if(response.videFile){
    	msg += `<video id="videoPlayer" src=${response.videoFile}></video><br><button id="videoCancel" onClick="videoCancel()">Video Audio</button><br><script>function videoCancel(){
         const videoCancel = document.getElementById("videoCancel");
@@ -31,9 +28,5 @@ app.get('/', (req, res) => {
    res.send(msg); 
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-
-})
 
 app.listen(4080)
